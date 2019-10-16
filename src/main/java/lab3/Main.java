fc88106322f7821fa61448ebdaf7e36b10e69d8c
Filonenko Ivan <199919991999@mail.ru>
2019-10-17 00:24:56 +0300

    Scripted auto-commit on change (2019-10-17 00:24:56) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -20,7 +20,8 @@ public class Main {
         final String header2 = schedule.first();
         schedule = schedule.filter(line -> !line.equals(header2));
         JavaPairRDD<Integer, String> airportsPair = airports.mapToPair(s -> new Tuple2<>(Integer.parseInt(s.split(",",2)[0]), s.split(",",2)[1]));
-        JavaPairRDD<Pair<Integer, Integer>, Pair<String,String>> SchedulePair = schedule.mapToPair(s -> new Tuple2<>(new Pair<>(Integer.parseInt(s.split(",")[11]),Integer.parseInt(s.split(",")[14])), new Pair<>(s.split(",")[17], s.split(",")[20])));
-        SchedulePair.saveAsTextFile(args[2]);
+        JavaPairRDD<Pair<Integer, Integer>, Pair<String,String>> schedulePair = schedule.mapToPair(s -> new Tuple2<>(new Pair<>(Integer.parseInt(s.split(",")[11]),Integer.parseInt(s.split(",")[14])), new Pair<>(s.split(",")[17], s.split(",")[20])));
+        schedulePair.
+        schedulePair.saveAsTextFile(args[2]);
     }
 }
