95cfcc269d9e9103464ef7a93550be96a26172d3
Filonenko Ivan <199919991999@mail.ru>
2019-10-16 23:48:39 +0300

    Scripted auto-commit on change (2019-10-16 23:48:39) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -14,7 +14,12 @@ public class Main {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airports = sc.textFile(args[0]).flatMap(s -> Arrays.stream(s.split("\t")).iterator()).filter(s -> {
-            if 
+            try {
+                Integer.parseInt(s.split(",", 2)[0]);
+                return true;
+            } catch (NumberFormatException e){
+                return false;
+            }
         });
         JavaRDD<String> schedule = sc.textFile(args[1]).flatMap(s -> Arrays.stream(s.split("\t")).iterator());
         JavaPairRDD<Integer, String> airportsPair = airports.mapToPair(s -> new Tuple2<>(Integer.parseInt(s.split(",",2)[0]), s.split(",",2)[1]));
