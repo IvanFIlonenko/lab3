41baea6da6aaa791c60db4e3a37348780e40471e
Filonenko Ivan <199919991999@mail.ru>
2019-10-17 02:22:55 +0300

    Scripted auto-commit on change (2019-10-17 02:22:55) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -30,11 +30,11 @@ public class Main {
         final String header2 = schedule.first();
         schedule = schedule.filter(line -> !line.equals(header2));
         JavaPairRDD<Integer, String> airportsPair = airports.mapToPair(s -> new Tuple2<>(Integer.parseInt(s.split(",",2)[0]), s.split(",",2)[1]));
-        JavaPairRDD<Pair<Integer, Integer>, int[]> schedulePair = schedule.mapToPair(s -> {
+        JavaPairRDD<Pair<Integer, Integer>, float[]> schedulePair = schedule.mapToPair(s -> {
             if (s.split(",")[17].length() > 0) {
-                return new Tuple2<>(new Pair<>(Integer.parseInt(s.split(",")[11]),Integer.parseInt(s.split(",")[14])), new int[]{Integer.parseInt(s.split(",")[17]),1,0,0});
+                return new Tuple2<>(new Pair<>(Integer.parseInt(s.split(",")[11]),Integer.parseInt(s.split(",")[14])), new float[]{Float.parseFloat(s.split(",")[17]),1,0,0});
             } else {
-                return new Tuple2<>(new Pair<>(Integer.parseInt(s.split(",")[11]),Integer.parseInt(s.split(",")[14])), new int[]{0,0,0,0});
+                return new Tuple2<>(new Pair<>(Integer.parseInt(s.split(",")[11]),Integer.parseInt(s.split(",")[14])), new float[]{0,0,0,0});
             }
         });
 //        JavaPairRDD<Pair<Integer, Integer>, String> schedulePair = schedule.mapToPair(s -> new Tuple2<>(new Pair<>(Integer.parseInt(s.split(",")[11]),Integer.parseInt(s.split(",")[14])), s.split(",")[17]));
