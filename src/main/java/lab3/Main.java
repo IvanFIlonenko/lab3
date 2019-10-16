cf65f7fcfbba64bdcb05e90e5d76fd5e08e16d8e
Filonenko Ivan <199919991999@mail.ru>
2019-10-16 20:46:22 +0300

    Scripted auto-commit on change (2019-10-16 20:46:22) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -1,6 +1,7 @@
 package lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
@@ -13,9 +14,9 @@ public class Main {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airports = sc.textFile(args[0]);
         JavaRDD<String> schedule = sc.textFile(args[1]);
-        JavaRDD<String> splitted = airports.flatMap(s -> Arrays.stream(s.split("\t")).iterator()).mapToPair(f-> {
-            return new Tuple2<String, String>(f.get(0))
-        });
+        JavaRDD<String> splitted = airports.flatMap(s -> Arrays.stream(s.split("\t")).iterator());
+        int count = 0;
+        JavaPairRDD<Integer, String> splittedCount = splitted.mapToPair(s -> new Tuple2<>(count++, s));
         airports.saveAsTextFile(args[2]);
     }
 }
