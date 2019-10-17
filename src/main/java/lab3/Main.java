337cddbae0fbceefe58b287431990d012de33451
Filonenko Ivan <199919991999@mail.ru>
2019-10-17 17:34:08 +0300

    Scripted auto-commit on change (2019-10-17 17:34:08) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -25,22 +25,22 @@ public class Main {
     private static final int NUMBER_OF_FLIGHTS_POS = 4;
 
 
-    public static JavaRDD<String> getDataFromFile(JavaSparkContext sc, String path){
+    private static JavaRDD<String> getDataFromFile(JavaSparkContext sc, String path){
         JavaRDD<String> data = sc.textFile(path).flatMap(s -> Arrays.stream(s.split("\t")).iterator());
         final String header = data.first();
         return data.filter(line -> !line.equals(header));
     }
 
-    public static String getFromAirports(int pos, String s){
+    private static String getFromAirports(int pos, String s){
         String s_sub = s.split(",", 2)[pos];
         return s_sub.substring(1,s_sub.length() - 1);
     }
 
-    public static String getFromSchedule(int pos, String s){
+    private static String getFromSchedule(int pos, String s){
         return s.split(",")[pos];
     }
 
-    public static String getAirportData(int pos, String s, boolean isAirports){
+    private static String getAirportData(int pos, String s, boolean isAirports){
         if (isAirports){
             return getFromAirports(pos, s);
         } else {
@@ -48,13 +48,13 @@ public class Main {
         }
     }
 
-    public static Broadcast<Map<Integer,String>> getAirportBroadcasted(JavaSparkContext sc, JavaRDD<String> airports){
+    private static Broadcast<Map<Integer,String>> getAirportBroadcasted(JavaSparkContext sc, JavaRDD<String> airports){
         JavaPairRDD<Integer, String> airportsPair = airports.mapToPair(s -> new Tuple2<>(Integer.parseInt(getAirportData(AIRPORT_ID_POS, s, true)), getAirportData(AIRPORT_NAME_POS, s, true)));
         Map<Integer, String> airportsMap = airportsPair.collectAsMap();
         return sc.broadcast(airportsMap);
     }
 
-    public static JavaPairRDD<Pair<Integer, Integer>, float[]> createSchedulePair(JavaRDD<String> schedule){
+    private static JavaPairRDD<Pair<Integer, Integer>, float[]> createSchedulePair(JavaRDD<String> schedule){
         return schedule.mapToPair(s -> {
             Pair<Integer, Integer> airportsIDs = new Pair<>(Integer.parseInt(getAirportData(AIRPORT_ID_FROM_POS,s,false)),Integer.parseInt(getAirportData(AIRPORT_ID_TO_POS,s,false)));
             float[] delayData = new float[]{0,0,0,0,1};
@@ -68,11 +68,11 @@ public class Main {
         });
     }
 
-    public static JavaPairRDD<Pair<Integer, Integer>, float[]> filterSchedule(JavaPairRDD<Pair<Integer, Integer>, float[]> schedule){
+    private static JavaPairRDD<Pair<Integer, Integer>, float[]> filterSchedule(JavaPairRDD<Pair<Integer, Integer>, float[]> schedule){
         return schedule.filter(pair -> pair._2[MAX_DELAY_POS] >= 0);
     }
 
-    public static JavaPairRDD<Pair<Integer, Integer>, float[]> reduceSchedule(JavaPairRDD<Pair<Integer, Integer>, float[]> schedule){
+    private static JavaPairRDD<Pair<Integer, Integer>, float[]> reduceSchedule(JavaPairRDD<Pair<Integer, Integer>, float[]> schedule){
         return schedule.reduceByKey((arr1,arr2) -> {
             arr1[NUMBER_OF_CANCELLED_POS] = arr1[NUMBER_OF_CANCELLED_POS] + arr1[IS_CANCELLED_POS] + arr2[IS_CANCELLED_POS] + arr2[NUMBER_OF_CANCELLED_POS];
             arr1[NUMBER_OF_DELAYED_POS] += arr2[NUMBER_OF_DELAYED_POS];
@@ -84,7 +84,7 @@ public class Main {
         });
     }
 
-    public static JavaPairRDD<Pair<Integer, Integer>, String> convertDataToString(JavaPairRDD<Pair<Integer, Integer>, float[]> schedule){
+    private static JavaPairRDD<Pair<Integer, Integer>, String> convertDataToString(JavaPairRDD<Pair<Integer, Integer>, float[]> schedule){
         return schedule.mapValues(arr -> "Max delay=" + arr[MAX_DELAY_POS] + "; Percent of delays = " + arr[NUMBER_OF_DELAYED_POS]/arr[NUMBER_OF_FLIGHTS_POS] * 100 +
                 "%; Percent of cancelled = " + arr[NUMBER_OF_CANCELLED_POS]/arr[NUMBER_OF_FLIGHTS_POS] * 100 + "%; Number of flights = " + arr[NUMBER_OF_FLIGHTS_POS]);
     }
