3438a9130e7843fee570ded6abbd2ab968fac708
Filonenko Ivan <199919991999@mail.ru>
2019-10-17 15:44:16 +0300

    Scripted auto-commit on change (2019-10-17 15:44:16) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -1,6 +1,7 @@
 package lab3;
 
 import javafx.util.Pair;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -9,10 +10,7 @@ import org.apache.spark.broadcast.Broadcast;
 import scala.Int;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static boolean isNumeric(String str) {
@@ -34,8 +32,12 @@ public class Main {
         final String header2 = schedule.first();
         schedule = schedule.filter(line -> !line.equals(header2));
         JavaPairRDD<Integer, String> airportsPair = airports.mapToPair(s -> new Tuple2<>(Integer.parseInt(s.split(",",2)[0]), s.split(",",2)[1]));
-        JavaRDD<Map<Integer, String >> kek = airports.map(s -> new Map<>(Integer.parseInt(s.split(",",2)[0]), s.split(",",2)[1]));
-        final Broadcast<Map<Integer,String>> airportsBroadcasted = sc.broadcast();
+        JavaRDD<Map<Integer, String >> kek = airports.map(s -> {
+            Map<Integer, String> temp = new HashMap<>();
+            temp.put(Integer.parseInt(s.split(",",2)[0]), s.split(",",2)[1])
+            return temp;
+        });
+        final Broadcast<Map<Integer,String>> airportsBroadcasted = sc.broadcast(kek);
         JavaPairRDD<Pair<Integer, Integer>, float[]> schedulePair = schedule.mapToPair(s -> {
             if (s.split(",")[17].length() > 0) {
                 return new Tuple2<>(new Pair<>(Integer.parseInt(s.split(",")[11]),Integer.parseInt(s.split(",")[14])), new float[]{Float.parseFloat(s.split(",")[17]),0,1,0,1});
