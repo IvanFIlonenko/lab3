ab64d4d2d13546ca0339a4b4280cd24ee52dc81c
Filonenko Ivan <199919991999@mail.ru>
2019-10-18 08:27:54 +0300

    Scripted auto-commit on change (2019-10-18 08:27:54) by gitwatch.sh

--- src/main/java/lab3/ScheduleFunctions.java
+++ src/main/java/lab3/ScheduleFunctions.java
@@ -3,6 +3,7 @@ package lab3;
 import javafx.util.Pair;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import scala.Tuple2;
 
 public class ScheduleFunctions {
 
@@ -10,11 +11,25 @@ public class ScheduleFunctions {
         return s.split(",")[pos];
     }
 
-    private static JavaPairRDD<Pair<Integer, Integer>, String> handleSchedule(JavaRDD<String> schedule){
+    public static JavaPairRDD<Pair<Integer, Integer>, String> handleSchedule(JavaRDD<String> schedule){
         JavaPairRDD<Pair<Integer, Integer>, float[]> schedulePair = createSchedulePair(schedule);
         JavaPairRDD<Pair<Integer, Integer>, float[]> scheduleFiltered = filterSchedule(schedulePair);
         JavaPairRDD<Pair<Integer, Integer>, float[]> scheduleReduced = reduceSchedule(scheduleFiltered);
         JavaPairRDD<Pair<Integer, Integer>, String> scheduleStringConverted = convertDataToString(scheduleReduced);
         return scheduleStringConverted;
+    }
+
+    private static JavaPairRDD<Pair<Integer, Integer>, float[]> createSchedulePair(JavaRDD<String> schedule){
+        return schedule.mapToPair(s -> {
+            Pair<Integer, Integer> airportsIDs = new Pair<>(Integer.parseInt(getAirportData(AIRPORT_ID_FROM_POS,s,false)),Integer.parseInt(getAirportData(AIRPORT_ID_TO_POS,s,false)));
+            float[] delayData = new float[]{0,0,0,0,1};
+            if (getAirportData(DELAY_FLIGHT_POS, s, false).length() > 0) {
+                delayData[MAX_DELAY_POS] = Float.parseFloat(getAirportData(DELAY_FLIGHT_POS,s,false));
+                delayData[NUMBER_OF_DELAYED_POS] = 1;
+            } else {
+                delayData[IS_CANCELLED_POS] = 1;
+            }
+            return new Tuple2<>(airportsIDs, delayData);
+        });
     }
 }
