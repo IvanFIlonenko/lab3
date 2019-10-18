175f41ecd0f416691c8934cef4adb9f8f1489bc5
Filonenko Ivan <199919991999@mail.ru>
2019-10-18 08:25:48 +0300

    Scripted auto-commit on change (2019-10-18 08:25:48) by gitwatch.sh

--- src/main/java/lab3/AirportsFunctions.java
+++ src/main/java/lab3/AirportsFunctions.java
@@ -1,4 +1,17 @@
 package lab3;
 
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+import scala.Tuple2;
+
+import java.util.Map;
+
 public class AirportsFunctions {
+    private static Broadcast<Map<Integer,String>> getAirportBroadcasted(JavaSparkContext sc, JavaRDD<String> airports){
+        JavaPairRDD<Integer, String> airportsPair = airports.mapToPair(s -> new Tuple2<>(Integer.parseInt(getAirportData(AIRPORT_ID_POS, s, true)), getAirportData(AIRPORT_NAME_POS, s, true)));
+        Map<Integer, String> airportsMap = airportsPair.collectAsMap();
+        return sc.broadcast(airportsMap);
+    }
 }
